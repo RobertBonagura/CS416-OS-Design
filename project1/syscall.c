94136b7544b29168b4b6a48ee55b9dd3ca98ee1e
Robert J. Bonagura <rjb348@butter.cs.rutgers.edu>
2020-02-06 14:04:49 -0500

    Add solution to part 3

--- project1/syscall.c
+++ project1/syscall.c
@@ -10,7 +10,7 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <sys/time.h>                                                                                
+#include <sys/time.h>
 #include <sys/syscall.h>
 
 double avg_time = 0;
@@ -18,11 +18,27 @@ double avg_time = 0;
 int main(int argc, char *argv[]) {
 
     /* Implement Code Here */
-
+	
     // Remember to place your final calculated average time
     // per system call into the avg_time variable
+    
+	struct timeval start;
+	struct timeval end;
 
-    printf("Average time per system call is %f microseconds\n", avg_time);
+	gettimeofday(&start, NULL);
+	int i = 0;
+	while (i < 5000000){
+		syscall(SYS_getuid);
+		i++;
+	}
+	gettimeofday(&end, NULL);
+	
+	float total_time = (end.tv_sec * 1000000 + end.tv_usec) -
+		(start.tv_sec * 1000000 + start.tv_usec);
+	
+	float avg_time = total_time / 5000000;
+
+	printf("Average time per system call is %1.4f microseconds\n", avg_time);
 
     return 0;
 }
